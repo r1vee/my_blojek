ef4930302a7c8c8140ad7fd1be39a7da08b94dda
Aleksey <r1ve225@gmail.com>
2017-11-11 02:16:03 +0300

    add facebook and vk login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable, :recoverable,
-  devise :database_authenticatable, :registerable, :rememberable, :trackable, :validatable
+  # :confirmable, :lockable, :timeoutable, :recoverable,
+  devise :database_authenticatable, :registerable, :rememberable, :trackable, 
+         :validatable, :omniauthable, omniauth_providers: [:facebook, :vkontakte]
   has_many :commentaries
   has_many :posts, dependent: :nullify
   # has_many :moderate_posts, class: 'Post', foreign_key: 'moderate_post_id'
   # has_one :commentary
-  devise :omniauthable, omniauth_providers: [:facebook]
+  
 
   validates_presence_of :name, :email
   # validates :name, length: { maximum: 16, minimum: 2 }
